60584617bd2c8b32be7cb421daba1eb1495e32b6
LS-KR <Elihuso@outlook.com>
2023-04-24 14:29:32 +0000

    [O] Delete unused value

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
-var isfinished;
-
 window.addEventListener('DOMContentLoaded', function() {
     var e = new XMLHttpRequest();
-    isfinished = false;
     e.open('get', 'https://raw.githubusercontent.com/FunctionSir/TransDefenseProject/master/%E6%88%92%E7%BD%91%E7%98%BE(%E5%8F%AF%E8%83%BD%E5%90%AB%E6%89%AD%E8%BD%AC%E6%B2%BB%E7%96%97)%E6%9C%BA%E6%9E%84%E5%88%97%E8%A1%A8.csv'),
         e.addEventListener('load',function() {
             var n = [],
@@ -52,8 +49,6 @@ window.addEventListener('DOMContentLoaded', function() {
                     b.addEventListener("click", (event) => {
                         ShowInfo(b.children.item(0).lastChild);
                     }, false);
-                    if (i == n.length - 1)
-                        isfinished = true;
                 }
             });
         }),
